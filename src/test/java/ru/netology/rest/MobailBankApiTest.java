7908bda32a6d6ac5572d8fafe771b60056facf5a
Сем Елена <semova.s81@bk.ru>
2022-10-08 13:52:59 +0300

    and test for postman is ok

--- src/test/java/ru/netology/rest/MobailBankApiTest.java
+++ src/test/java/ru/netology/rest/MobailBankApiTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
-public class forPostmanTest {
+public class MobailBankApiTest {
 
     @Test
     void forPostmanTest() {
@@ -15,15 +15,16 @@ public class forPostmanTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .contentType("text/plain; charset=utf-8")
+                .body("some data")
+// отправляемые данные (заголовки и query можно выставлять аналогично)  Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"))
         ;
     }
 }
-
